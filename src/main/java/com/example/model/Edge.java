8a5b1fcb99c77af8014f75b63590322e533aa2e9
Conor-20105865 <147147702+Conor-20105865@users.noreply.github.com>
2025-05-06 10:42:16 +0100

    static fix

--- src/main/java/com/example/model/Edge.java
+++ src/main/java/com/example/model/Edge.java
@@ -3,10 +3,10 @@ package com.example.model;
 public class Edge {
 
     //Declare Variables
-    private static Station from;
-    private static Station to;
-    private static double distance;
-    private static String line;
+    public  Station from;
+    public  Station to;
+    public  double distance;
+    public  String line;
 
     //Edge Class Constructor
     public Edge(Station from, Station to, double distance, String line) {
